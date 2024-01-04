0fc4d289d864788bb371716a7de18cbcc96e849b
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-03 20:51:05 -0600

    add test case failure

--- react-with-api/src/reducers/Top-Stories-Reducer.js
+++ react-with-api/src/reducers/Top-Stories-Reducer.js
@@ -8,4 +8,15 @@ const topStoriesReducer = (state = { isLoaded: false, topStories: [], error: nul
         isLoaded: true,
         topStories: action.topStories
       };
-    }};
+    case c.GET_TOP_STORIES_FAILURE:
+      return {
+        ...state,
+        isLoaded: true,
+        error: action.error
+      };
+    default:
+      return state;
+  }
+};
+
+export default topStoriesReducer;
